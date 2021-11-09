f5caba4cedb60084d9413171350e2b0f6f9a4956
Sertac Ayhan <srtc_ayhan@icloud.com>
2021-11-09 15:05:02 +0300

    map kit api key and my location implemented

--- app/src/main/java/com/example/srtcayhan/hmsaccountkit/HmsAccountKit.kt
+++ app/src/main/java/com/example/srtcayhan/hmsaccountkit/HmsAccountKit.kt
@@ -2,11 +2,13 @@ package com.example.srtcayhan.hmsaccountkit
 
 import android.app.Application
 import com.huawei.hms.ads.HwAds
+import com.huawei.hms.maps.MapsInitializer
 
 class HmsAccountKit : Application() {
     override fun onCreate() {
         super.onCreate()
         // Initialize the Ads SDK.
         HwAds.init(this)
+        MapsInitializer.setApiKey("CwEAAAAAJo5BV02u3Cu2mxr18WIZKXpQEUF7jSI1oLZOHqvSlmKg1Mg0k8we0IiGORkM0hrIhBauG2vMz6Hw25PNoETSsNzqIYA=")
     }
 }
--- app/src/main/java/com/example/srtcayhan/hmsaccountkit/map/MapActivity.kt
+++ app/src/main/java/com/example/srtcayhan/hmsaccountkit/map/MapActivity.kt
@@ -1,17 +1,18 @@
 package com.example.srtcayhan.hmsaccountkit.map
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.Manifest.permission.ACCESS_WIFI_STATE
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.srtcayhan.hmsaccountkit.R
-import com.huawei.hms.maps.CameraUpdateFactory
-import com.huawei.hms.maps.HuaweiMap
-import com.huawei.hms.maps.MapView
-import com.huawei.hms.maps.OnMapReadyCallback
+import com.huawei.hms.maps.*
 import com.huawei.hms.maps.model.LatLng
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -29,6 +30,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate:")
         super.onCreate(savedInstanceState)
+        MapsInitializer.setApiKey("CwEAAAAAJo5BV02u3Cu2mxr18WIZKXpQEUF7jSI1oLZOHqvSlmKg1Mg0k8we0IiGORkM0hrIhBauG2vMz6Hw25PNoETSsNzqIYA=")
         setContentView(R.layout.activity_map)
         mMapView = findViewById(R.id.mapView)
         var mapViewBundle: Bundle? = null
@@ -40,19 +42,31 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             onCreate(mapViewBundle)
             getMapAsync(this@MapActivity)
         }
+        hasPermissions()
     }
 
+    @RequiresPermission(allOf = [ACCESS_FINE_LOCATION, ACCESS_WIFI_STATE])
     override fun onMapReady(map: HuaweiMap) {
-        Log.d(TAG, "onMapReady: ")
         hMap = map
+        // Enable the my-location layer.
+        hMap!!.isMyLocationEnabled = true
+        // Enable the my-location icon.
+        hMap!!.uiSettings.isMyLocationButtonEnabled = true
     }
 
-    private fun hasPermissions(context: Context, vararg permissions: String): Boolean {
+    private fun hasPermissions(): Boolean {
+        // If the API level is 23 or higher (Android 6.0 or later), you need to dynamically apply for permissions.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            for (permission in permissions) {
-                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-                    return false
-                }
+            Log.i(TAG, "sdk >= 23 M")
+            // Check whether your app has the specified permission and whether the app operation corresponding to the permission is allowed.
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // Request permissions for your app.
+                val strings = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+                // Request permissions.
+                ActivityCompat.requestPermissions(this, strings, 1)
             }
         }
         return true
